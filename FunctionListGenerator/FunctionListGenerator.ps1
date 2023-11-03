e402ae5f5b629617546fb406b20b5b21ce10ef6f
Angelica Wisnes <roger.angelica.wisnes@dnb.no>
2023-11-03 12:45:55 +0100

    Various small changes

--- FunctionListGenerator/FunctionListGenerator.ps1
+++ FunctionListGenerator/FunctionListGenerator.ps1
@@ -40,7 +40,7 @@ function Initialize-FunctionListGenerator {
   | ForEach-Object { @{ $_.Key = $_.Value } }
   
   foreach ($listObject in $global:FunctionLists.Values) { 
-    $listObject = [System.Collections.Generic.List[FunctionListElement]]($listObject | Sort-Object -Property value)
+    $listObject = [System.Collections.Generic.List[FunctionListElement]]($listObject | Sort-Object -Property:value)
     $FunctionList_single.AddRange( $listObject )
     
     $diff = $FunctionList_Dual_Col1.Count - $FunctionList_Dual_Col2.Count
@@ -54,12 +54,10 @@ function Initialize-FunctionListGenerator {
   $FunctionList_Dual_Col2.Add( $FunctionSubList_End ) 
   
   # Define helper-variables for function-list-generation
-  $global:categoryWidth = (($FunctionList_single.category) | Measure-Object -Maximum -Property Length).Maximum + 1
-  $global:nameWidth = (($FunctionList_single.name) | Measure-Object -Maximum -Property Length).Maximum + 1
-  $global:valueWidth = (($FunctionList_single.value) | Measure-Object -Maximum -Property Length).Maximum + 1
+  $global:categoryWidth = (($FunctionList_single.category) | Measure-Object -Maximum -Property:Length).Maximum + 1
+  $global:nameWidth = (($FunctionList_single.name) | Measure-Object -Maximum -Property:Length).Maximum + 1
+  $global:valueWidth = (($FunctionList_single.value) | Measure-Object -Maximum -Property:Length).Maximum + 1
   $global:fullWidth = $global:categoryWidth + $global:nameWidth + $global:valueWidth + 5
-  $global:total_width_single = $global:fullWidth + 3
-  $global:total_width_dual = $global:total_width_single * 2
 }
 
 
@@ -75,27 +73,19 @@ function Add-BlankLinesToDualLists {
   For ($i = 0; $i -lt $col2_Len; $i++) { $FunctionList_Dual_Col2.Add( $FunctionSubList_Empty ) }
 }
 
-function FormatStringw([string]$str, [int]$colWidth, [string]$fillerChar, [switch]$NoPadding) { 
-  $padding = If ($NoPadding) { "" } Else { $fillerChar }
-  $x = If ($NoPadding) { 1 } Else { 0 }
-  $fillerLength = ($colWidth - $x) - (($padding + $str).Length)
-  Return $padding + $str + ($fillerChar * $fillerLength) + $padding 
-}
-
 function FormatString([string]$str, [int]$colWidth, [string]$fillerChar, [switch]$NoPadding) { 
   $padding = If ($NoPadding) { "" } Else { $fillerChar }
   return $padding + $str.PadRight($colWidth-1, $fillerChar) + $padding 
 }
 
 
-
 function FormatElement([FunctionListElement]$element, [switch]$NoPadding) {
   $fillerChar = If ($element.value -eq "_") { "_" } elseif ($element.value -eq "-") { "-" } Else { " " }
-
+  
   Return "|{0}|{1}|{2}|" -f `
-    (FormatString -str:$element.category -colWidth:$global:categoryWidth -fillerChar:$fillerChar -NoPadding:$NoPadding),
-    (FormatString -str:$element.name -colWidth:$global:nameWidth -fillerChar:$fillerChar -NoPadding:$NoPadding),
-    (FormatString -str:$element.value -colWidth:$global:valueWidth -fillerChar:$fillerChar -NoPadding:$NoPadding)
+  (FormatString -str:$element.category -colWidth:$global:categoryWidth -fillerChar:$fillerChar -NoPadding:$NoPadding),
+  (FormatString -str:$element.name -colWidth:$global:nameWidth -fillerChar:$fillerChar -NoPadding:$NoPadding),
+  (FormatString -str:$element.value -colWidth:$global:valueWidth -fillerChar:$fillerChar -NoPadding:$NoPadding)
 }
 
 
@@ -107,29 +97,31 @@ function Add-ToFunctionList {
     [Parameter(Mandatory)][String]$category,
     [Parameter(Mandatory)][String]$name,
     [Parameter(Mandatory)][String]$value
-  )
-  $global:FunctionLists[$category].Add(( [FunctionListElement]@{ category = $category; name = $name; value = $value } ))
-}
-
-function Get-ListOfFunctionsAndAliases {
+    )
+    $global:FunctionLists[$category].Add(( [FunctionListElement]@{ category = $category; name = $name; value = $value } ))
+  }
+  
+  function Get-ListOfFunctionsAndAliases {
+  $global:total_width_single = $global:fullWidth + 3
+  $global:total_width_dual = $global:total_width_single * 2
   $windowWidth, $_ = Get-WindowDimensions
   $isDual = $global:total_width_dual -lt $windowWidth
   $isSingleWithPadding = $global:total_width_single -lt $windowWidth
   $isSingleNoPadding = (-not $isDual) -and (-not $isSingleWithPadding)
   $widthAdjustment = If ($isSingleNoPadding) { 6 } Else { 0 }
 
-  $sb = [System.Text.StringBuilder]::new("AWI-defined functions and aliases:")
-  $newLine = If ($isSingleNoPadding) { "`n" } Else { "`n  " }
+  $sb = [System.Text.StringBuilder]::new("AWI-defined functions and aliases:`n")
+  $indent = If ($isSingleNoPadding) { "" } Else { "  " }
 
   If ($isDual) { 
-    [void]$sb.AppendFormat("$newLine {0}   {0}$newLine", ("_" * ($global:fullWidth)))
+    [void]$sb.AppendFormat("$indent.{0}. .{0}.`n", ("_" * ($global:fullWidth)))
     for ($i = 0; $i -lt $FunctionList_Dual_Col1.Count; $i++) { 
-      [void]$sb.AppendFormat("{0} {1}$newLine", (FormatElement -element:$FunctionList_Dual_Col1[$i]), (FormatElement -element:$FunctionList_Dual_Col2[$i]))
+      [void]$sb.AppendFormat("$indent{0} {1}`n", (FormatElement -element:$FunctionList_Dual_Col1[$i]), (FormatElement -element:$FunctionList_Dual_Col2[$i]))
     }
   }
   Else { 
-    [void]$sb.AppendFormat("$newLine {0}$newLine", ("_" * ($global:fullWidth - $widthAdjustment)))
-    $FunctionList_single | ForEach-Object { [void]$sb.AppendFormat("{0}$newLine", (FormatElement -element:$_ -NoPadding:$isSingleNoPadding)) }
+    [void]$sb.AppendFormat("$indent.{0}.`n", ("_" * ($global:fullWidth - $widthAdjustment)))
+    $FunctionList_single | ForEach-Object { [void]$sb.AppendFormat("$indent{0}`n", (FormatElement -element:$_ -NoPadding:$isSingleNoPadding)) }
   }
 
   OUT $(PE -txt:$sb.ToString() -fg:$global:colors.DeepPink)
